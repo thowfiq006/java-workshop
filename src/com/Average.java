d162ae35a3210c18fae4f61ab7e11098a853d1ba
thowfq006 <veerapandykatabomabn@gmail.com>
2025-07-02 15:02:35 +0530

    Average

---
+++ src/com/Average.java
@@ -0,0 +1,8 @@
+public class Average { 
+    public static void main(String[] args) {
+        int  a=87,b=70,c=98,d=60;
+        int total  = a+b+c+d+e;
+        System.out.println("total:"+total);
+    }
+    
+}
---
+++ src/com/vetias/java/workshop/Mark.java
@@ -0,0 +1,7 @@
+public class Mark {
+ public static void main(String[] args) {
+    int [] mark=(20,30,40,50,60,70);
+    for(int mark:markes){
+        System.out.println(mark);      
+    }
+}
