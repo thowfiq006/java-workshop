5f0b842732085a34dc9012eb18c0d165be291585
thowfq006 <veerapandykatabomabn@gmail.com>
2025-07-04 09:38:26 +0530

    https://github.com/thowfiq006/coding.git

---
+++ src/com/src/com/vetias/java/workshop/tempdata/beans/bulding.java
@@ -0,0 +1,32 @@
+package src.com.vetias.java.workshop.tempdata.beans;
+
+public class bulding {
+    public static void main(String[] args) {
+        private String name;
+        private double area;
+        private double floor;
+        private double localdatabase;
+        private double opendatatime;
+}
+
+public String sentname(String Name) {
+    THIS.name =Name;
+}
+public String getname(){
+    return name;
+}
+public String setarea(String area){
+    this.area =area
+}
+public String getarea(){
+    return area;
+}
+public String setfloor (String floor ){
+    floor = floor
+}
+public String getfloor(){
+    return localdatatime;
+}
+public String setlocaldatatime(String opendatatime){
+    opendatatime = opendatatime
+}
---
+++ src/com/src/com/vetias/java/workshop/tempdata/beans/hostal.java
@@ -0,0 +1,6 @@
+package src.com.vetias.java.workshop.tempdata.beans;
+
+public class hostal exetention  Bulding {
+private boolean ismess avalible
+
+}
