024b855a44e57bbac8e98833fee70cd5f4ad2dc1
thowfq006 <veerapandykatabomabn@gmail.com>
2025-07-08 15:02:44 +0530

    have it

--- src/com/src/com/vetias/java/workshop/tempdata/beans/AgeCalculator.java
+++ src/com/src/com/vetias/java/workshop/tempdata/beans/AgeCalculator.java
@@ -1,4 +1,5 @@
 package com.vetias.java.workshop.tempdata.beans;
+import java.time.LocalDate;
 import java.util.Scanner;
     public class AgeCalculator {
 
@@ -9,5 +10,12 @@ import java.util.Scanner;
             int currentYear = 2025;
             int age = currentYear - yearOfBirth;
             System.out.println("Age: " + age);
+
+            System.out.println("Enter your month of birth (1-12):");
+            String monthOfBirth = scanner.next();
+            LocalDate birthDate = LocalDate.parse(dateOfBirth);
+            int extractedYearOfBirth = birthDate.getYear();
+            int extractedMonthOfBirth = birthDate.getMonthValue();
+            System.out.println("Enter your day of birth (1-31):"); 
         }
  }
